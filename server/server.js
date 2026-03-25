69ac433f6bd7adc481f272c3538ea73a850cc8d1
Alok Singh <ialksng@gmail.com>
2026-03-25 05:49:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,4 +21,6 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
