daf6d01c5636c295e460d30addd149d8375c3d11
Alok Singh <ialksng@gmail.com>
2026-04-10 04:01:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/projects/smartsphere/', 
-  build: {
-    outDir: 'dist',
-  }
-})
+  plugins: [react()],
+  base: '/projects/smartsphere/',
+});
