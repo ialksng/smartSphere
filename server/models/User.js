29614c16b0030e37ecfd8b4328d8aa25d3f1bf0b
Alok Singh <ialksng@gmail.com>
2026-04-10 05:14:18 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -9,11 +9,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+    // If the password hasn't been modified, just return to let Mongoose continue
+    if (!this.isModified('password')) return; 
+    
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // Method to compare passwords
