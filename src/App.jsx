aeaecde6034f8c8dae1bf7203e358f259ae3b3ab
Alok Singh <ialksng@gmail.com>
2026-04-20 02:25:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const ProtectedLayout = ({ children }) => (
 
 function App() {
   return (
-    <BrowserRouter basename="/projects/smartsphere">
+    <BrowserRouter>
       <Routes>
 
         <Route path="/" element={<Navigate to="/auth" replace />} />
